80cb9d3de66aedd5c8fa96fa39d1bf8abbac02f5
Sylvain Olengha <solengha@vodfactory.com>
2023-07-21 00:31:45 +0200

    remove unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+import { ClerkProvider } from '@clerk/nextjs';
+import type { Metadata } from 'next';
+import { Montserrat } from 'next/font/google';
+
+const font = Montserrat({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Livepixelz",
-  description: "AI Platform for Developers",
+  title: 'Livepixelz',
+  description: 'AI Platform for Developers',
 };
 
 export default function RootLayout({
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={font.className}>{children}</body>
       </html>
     </ClerkProvider>
   );
