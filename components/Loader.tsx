a029ab963486df100d5cededce5dd30ac7446f6c
Sylvain Olengha <solengha@vodfactory.com>
2023-07-21 09:16:07 +0200

    Implementation of Image Prompt

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,14 +1,11 @@
 import Image from 'next/image';
+import Logo from '@/components/Logo';
 
 const Loader = () => {
   return (
     <div className="h-full flex flex-col gap-y-8 items-center justify-center">
       <div className="w-10 h-10 relative animate-pulse">
-        <Image
-          alt="logo"
-          fill
-          src="/images/logo_dark.png"
-        />
+        <Logo />
       </div>
       <p className="text-sm text-muted-foreground"> Livepixelz is working...</p>
     </div>
---
+++ components/Logo.tsx
@@ -0,0 +1,21 @@
+const Logo = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+      viewBox="0 0 240 240"
+    >
+      <path
+        style={{ fill: 'currentColor' }}
+        d="M120 0A120 120 0 0 1 0-120a120 120 0 0 1 120-120 120 120 0 0 1 120 120A120 120 0 0 1 120 0zm0-30a90 90 0 0 0 90-90 90 90 0 0 0-90-90 90 90 0 0 0-90 90 90 90 0 0 0 90 90z"
+        transform="scale(1 -1)"
+      />
+      <path
+        style={{ fill: 'currentColor' }}
+        d="M120 78a60 60 0 0 0-60 60 60 60 0 0 0 60 60 60 60 0 0 0 60-60 60 60 0 0 0-60-60zm-35 27h20v20H85v10H75v-10h10v-20z"
+      />
+    </svg>
+  );
+};
+
+export default Logo;
